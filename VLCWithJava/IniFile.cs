913f6a132a9993fff6b1ea9d1ab1d0f2dc1be5b9
thepwrtank18 <pt18.relay@gmail.com>
2024-08-02 01:14:34 -0500

    Add project files.

---
+++ VLCWithJava/IniFile.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+// Change this to match your program's normal namespace
+namespace VLCWithJava
+{
+    partial class IniFile   // revision 11
+    {
+        private readonly string _path;
+        private readonly string _exe = Assembly.GetExecutingAssembly().GetName().Name ?? throw new InvalidOperationException();
+
+        [LibraryImport("kernel32", EntryPoint = "WritePrivateProfileString", SetLastError = true, StringMarshalling = StringMarshalling.Utf16)]
+        private static partial long WritePrivateProfileString(string section, string? key, string? value, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        private static extern int GetPrivateProfileString(string section, string key, string @default, StringBuilder retVal, int size, string filePath);
+
+        public IniFile(string? iniPath = null)
+        {
+            _path = new FileInfo(iniPath ?? _exe + ".ini").FullName;
+        }
+
+        public string Read(string key, string? section = null)
+        {
+            var retVal = new StringBuilder(255);
+            GetPrivateProfileString(section ?? _exe, key, "", retVal, 255, _path);
+            return retVal.ToString();
+        }
+
+        public void Write(string? key, string? value, string? section = null)
+        {
+            WritePrivateProfileString(section ?? _exe, key, value, _path);
+        }
+
+        public void DeleteKey(string? key, string? section = null)
+        {
+            Write(key, null, section ?? _exe);
+        }
+
+        public void DeleteSection(string? section = null)
+        {
+            Write(null, null, section ?? _exe);
+        }
+
+        public bool KeyExists(string key, string? section = null)
+        {
+            return section != null && Read(key, section).Length > 0;
+        }
+    }
+}
---
+++ VLCWithJava/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Diagnostics;
+
+namespace VLCWithJava
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!File.Exists("java_config.ini"))
+            {
+                try
+                {
+                    File.Create("java_config.ini").Dispose();
+                    // Write the default values to the ini file
+                    IniFile ini = new("java_config.ini");
+                    ini.Write("JAVA_HOME", "[vlcdir]\\java", "Config");
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.WriteLine("You need to run this program as an Administrator at least once.");
+                    Environment.Exit(1);
+                }
+            }
+
+            // get directory of the current executable
+            string currentDir = AppContext.BaseDirectory;
+
+            // Read the ini file
+            IniFile iniFile = new("java_config.ini");
+            string javaHome = iniFile.Read("JAVA_HOME", "Config").Replace("[vlcdir]", currentDir);
+            // set environment variable
+            Environment.SetEnvironmentVariable("JAVA_HOME", javaHome);
+            Environment.SetEnvironmentVariable("JRE_HOME", javaHome);
+            // set the PATH variable (to avoid conflicts with other Java installations, overwrite the PATH variable)
+            Environment.SetEnvironmentVariable("PATH", javaHome);
+
+            string[] modifiedArgs = args;
+
+            // check if there's "--started-from-file" in the arguments
+            if (args.Length > 0 && args[0] == "--started-from-file")
+            {
+                string fileProtocol = args[1];
+                Console.WriteLine(fileProtocol);
+                // does it say "dvd:///" at the beginning?
+                if (fileProtocol.StartsWith("dvd:///"))
+                {
+                    // if it does, let's check if it's actually a blu-ray disc
+                    string filePath = fileProtocol.Replace("dvd:///", "");
+                    // despite saying 'file', it's actually a directory
+                    if (Directory.Exists(filePath))
+                    {
+                        // check if there's a BDMV folder
+                        if (Directory.Exists(Path.Combine(filePath, "BDMV")))
+                        {
+                            // if there is, it's a blu-ray disc
+                            modifiedArgs[1] = fileProtocol.Replace("dvd:///", "bluray:///");
+                            Console.WriteLine(modifiedArgs[1]);
+                        }
+                    }
+                }
+            }
+
+
+            // start VLC
+            Process vlc = new();
+            vlc.StartInfo.FileName = "vlc.exe";
+            vlc.StartInfo.Arguments = string.Join(" ", modifiedArgs);
+            vlc.Start();
+            Environment.Exit(0);
+        }
+    }
+}
